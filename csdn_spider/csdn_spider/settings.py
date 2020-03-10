166c936948be9eeca4b43167223e1db6534b8b9f
殷龙 <y85171642@gmail.com>
2020-03-10 14:29:08 +0800

    settings

--- csdn_spider/csdn_spider/settings.py
+++ csdn_spider/csdn_spider/settings.py
@@ -21,17 +21,17 @@ NEWSPIDER_MODULE = 'csdn_spider.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.01
+DOWNLOAD_DELAY = 0.02
 DOWNLOAD_TIMEOUT = 5
 
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 32
-CONCURRENT_REQUESTS_PER_IP = 32
+# CONCURRENT_REQUESTS_PER_DOMAIN = 32
+# CONCURRENT_REQUESTS_PER_IP = 32
 
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
